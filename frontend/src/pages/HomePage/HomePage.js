d5aa9ddf96ae66af4489eaad88e25e8ca5b3c58c
Mitch Olson <mitch3344@gmail.com>
2022-05-17 18:43:12 -0500

    Able to display chefs along with their posts, and dishes available

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -13,43 +13,32 @@ const HomePage = (props) => {
   async function getChefs() {
     // console.log(user._id);
     let response = await axios.get(
-      `http://localhost:3012/api/users/${user._id}`
+      `http://localhost:3012/api/users/chefs`
     );
-    console.log(response.data);
-    setUserChefs(response.data.chefs);
+    // console.log(response.data);
+    setUserChefs(response.data);
   }
-  async function getChefDishes() {
-    setChefs([]);
-    for (let i = 0; i < userChefs.length; i++) {
-      await axios
-        .get(`http://localhost:3012/api/users/${userChefs[i]}/chefs`)
-        .then((response) => {
-          //console.log(response.data)
-          setChefs((chef) => [...chef, response.data]);
-        });
-    }
-  }
+  // async function getChefDishes() {
+  //   setChefs([]);
+  //   for (let i = 0; i < userChefs.length; i++) {
+  //     await axios
+  //       .get(`http://localhost:3012/api/users/${userChefs[i]}/chefs`)
+  //       .then((response) => {
+  //         //console.log(response.data)
+  //         setChefs((chef) => [...chef, response.data]);
+  //       });
+  //   }
+  // }
 
-  const flatArray = Chefs.flatMap((item) => item);
-  //console.log("flat", flatArray)
-
-  const convertDate = flatArray
-    .map(
-      (item) => ({ ...item, sortDate: new Date(item.dateAdded) })
-      // console.log( new Date(item.dateAdded));
-      // item.dateAdded = Date.parse(item.dateAdded)
-    )
-    .sort((date1, date2) => date2.sortDate - date1.sortDate);
-  //console.log(convertDate)
-
+  
   useEffect(() => {
     getChefs();
   }, []);
 
-  useEffect(() => {
-    getChefDishes();
-    //console.log(Chefs);
-  }, [userChefs]);
+  // useEffect(() => {
+  //   getChefDishes();
+  //   // console.log(Chefs);
+  // }, [userChefs]);
 
   return (
     <div className="container">
@@ -58,19 +47,21 @@ const HomePage = (props) => {
       <thead>
         <tr>
           <td>Chef</td>
+          <td></td>
           <td>Dishes</td>
           <td>{" "}</td>
         </tr>
       </thead>
       <tbody>
-        {convertDate &&
-          convertDate.map((f, i) => {
+        {userChefs &&
+          userChefs.map((f, i) => {
             return (
               <tr key={i}>
                 <td>{f.chef}</td>
+                <td>{f.post}</td>
                 <td>{f.dishes}</td>
                 <td>
-                  <LikeDislike f={f} getChefDishes={getChefDishes} />
+                  {/* <LikeDislike f={f} getChefDishes={getChefDishes} /> */}
                 </td>
               </tr>
             );

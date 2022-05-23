1c58cbcf45b0318c670211d60518d7a499dee7dc
Mitch Olson <mitch3344@gmail.com>
2022-05-23 11:32:06 -0500

    Updated HomePage to allow a user to select a chef and display partial data of what is to offer

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,72 +5,111 @@ import AuthContext from "../../context/AuthContext";
 import LikeDislike from "../../components/LikeDislike/LikeDislike";
 
 const HomePage = (props) => {
-  const { user } = useContext(AuthContext);
-  const [allChefPosts, setallChefPosts] = useState([]);
-  const [Chefs, setChefs] = useState([]);
+  const { user, chef } = useContext(AuthContext);
+
   const [userChefs, setUserChefs] = useState([]);
   const [image, SetImage] = useState([]);
+  const [chefClicked, setChefClicked] = useState(false);
+  const [selectedChef, setSelectedChef] = useState([]);
+  const [foundChef, setFoundChef] = useState([])
+
 
   async function getChefs() {
     // console.log(user._id);
     let response = await axios.get(
       `http://localhost:3012/api/users/chefs`
     );
-    // console.log(response.data);
+    console.log(response.data);
+    userChefs.map((chef) => (
+      console.log(chef._id)
+    )); 
     setUserChefs(response.data);
   }
-  async function getChefInfo (event) {
-    event.preventDefault();
-    setChefs([]);
-    for (let i = 0; i < userChefs.length; i++) {
-      await axios
-        .get(`http://localhost:3012/api/users/${userChefs[i]}/chefs`)
-        .then((response) => {
-          //console.log(response.data)
-          setChefs((chef) => [...chef, response.data]);
-        });
+
+
+    function displayChef(e) {
+      e.preventDefault();
+      console.log(e.target.innerText)
+      setChefClicked(true)
+      // setSelectedChef(i)
+      let name = e.target.innerText;
+      let foundChef = userChefs.find(e => e.chef === name)
+      setFoundChef(foundChef)
+      // console.log("foundChef", foundChef)
     }
-  }
+
+  
+  // async function handleClickInfo (event) {
+  //   event.preventDefault();
+  //   setChefs([]);
+  //     await axios
+  //       .get(`http://localhost:3012/api/users/${userChefs[chef._id]}`)
+  //       .then((response) => 
+  //         //console.log(response.data)
+  //         setChefs((chef) => [...chef, response.data])
+  //       );
+  //       console.log(chef)
+    
+  // }
 
   
   useEffect(() => {
     getChefs();
+    console.log()
   }, []);
 
   // useEffect(() => {
-  //   getChefDishes();
+  //   displayChef();
   //   // console.log(Chefs);
-  // }, [userChefs]);
+  // }, []);
 
+  // if (chefClicked === true){
+  //   console.log(userChefs)
+  //   return(
+  //   <div className="container">
+  //     {userChefs.map((chef, i) => (
+  //       <li key={i}>
+  //         {chef.meal}
+  //       </li>
+  //     ))}
+  //   </div>
+  //   )
+  // }
+  if (chefClicked === true){
+    return(
+      <div className="container">
+        <h1>{foundChef.dishes}</h1>
+        <h2></h2>
+      </div>
+    )
+  }
+  else{
+
+  
   return (
     <div className="container">
      <h2>Home Page for {user.name}!</h2>
-    <table className="table">
-      <thead>
-        <tr>
-          <td>CHEFS</td>
-          <td></td>
-          <td>{" "}</td>
-        </tr>
-      </thead>
-      <tbody>
-        {userChefs &&
-          userChefs.map((f, i) => {
-            return (
-              <tr key={i}>
-                <td>{f.chef}</td>
-                {/* <td>{f.post}</td>
-                <td>{f.dishes}</td> */}
-                <td>
-                  {/* <LikeDislike f={f} getChefDishes={getChefDishes} /> */}
-                </td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+        {userChefs && 
+          userChefs.map((chef, i) => (
+            <li key={i}>
+              <button onClick={displayChef}>                
+                {chef.chef}        
+              </button>
+            </li>        
+          ))}
+
+            {/* {(chefClicked === true) && 
+                userChefs.map((chef, i) => (
+                <li key={i}>
+                  {chef.meal}
+                </li>
+              ))
+            } */}
+          
+         
     </div>
   );
+  }
 };
 
 
